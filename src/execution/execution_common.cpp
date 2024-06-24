736da07c4b3d1e346360115ce95acca6ed47fedf
TheDesertedSea <745468443@qq.com>
2024-06-24 15:49:10 -0400

    p4: completed t1, t2

--- src/execution/execution_common.cpp
+++ src/execution/execution_common.cpp
@@ -17,6 +17,7 @@ namespace bustub {
 
 auto ReconstructTuple(const Schema *schema, const Tuple &base_tuple, const TupleMeta &base_meta,
                       const std::vector<UndoLog> &undo_logs) -> std::optional<Tuple> {
+  /** Initial data */
   std::vector<Value> values;
   auto is_deleted = base_meta.is_deleted_;
   for (uint32_t idx = 0; idx < schema->GetColumnCount(); idx++) {
@@ -24,6 +25,7 @@ auto ReconstructTuple(const Schema *schema, const Tuple &base_tuple, const Tuple
   }
 
   for (auto &undo_log : undo_logs) {
+    /** Generate partial schema */
     std::vector<Column> modified_columns;
     std::vector<uint32_t> modified_idxs;
     for (uint32_t idx = 0; idx < schema->GetColumnCount(); idx++) {
@@ -34,6 +36,7 @@ auto ReconstructTuple(const Schema *schema, const Tuple &base_tuple, const Tuple
     }
     auto partial_schema = Schema(modified_columns);
 
+    /** Apply undo */
     for (uint32_t i = 0; i < modified_idxs.size(); i++) {
       values[modified_idxs[i]] = undo_log.tuple_.GetValue(&partial_schema, i);
     }
@@ -52,10 +55,48 @@ void TxnMgrDbg(const std::string &info, TransactionManager *txn_mgr, const Table
   // always use stderr for printing logs...
   fmt::println(stderr, "debug_hook: {}", info);
 
-  fmt::println(
-      stderr,
-      "You see this line of text because you have not implemented `TxnMgrDbg`. You should do this once you have "
-      "finished task 2. Implementing this helper function will save you a lot of time for debugging in later tasks.");
+  auto itr = table_heap->MakeIterator();
+  while (!itr.IsEnd()) {
+    auto [tuple_meta, tuple] = itr.GetTuple();
+    auto rid = itr.GetRID();
+    auto ts = (tuple_meta.ts_ & TXN_START_ID) != 0 ? "txn" + std::to_string(tuple_meta.ts_ ^ TXN_START_ID)
+                                                   : std::to_string(tuple_meta.ts_);
+    fmt::print(stderr, "RID={}/{}, ts={} {} tuple={}\n", rid.GetPageId(), rid.GetSlotNum(), ts,
+               tuple_meta.is_deleted_ ? "<del>" : "", tuple.ToString(&table_info->schema_));
+
+    /** Initial Values */
+    std::vector<Value> values;
+    for (uint32_t idx = 0; idx < table_info->schema_.GetColumnCount(); idx++) {
+      values.push_back(tuple.GetValue(&table_info->schema_, idx));
+    }
+    auto undo_link = txn_mgr->GetUndoLink(rid);
+    while (undo_link.has_value() && undo_link->IsValid()) {
+      auto undo_log = txn_mgr->GetUndoLog(undo_link.value());
+      auto txn_id = undo_link->prev_txn_ ^ TXN_START_ID;
+
+      /** Generate partial schema */
+      std::vector<Column> modified_columns;
+      std::vector<uint32_t> modified_idxs;
+      for (uint32_t idx = 0; idx < table_info->schema_.GetColumnCount(); idx++) {
+        if (undo_log.modified_fields_[idx]) {
+          modified_columns.push_back(table_info->schema_.GetColumn(idx));
+          modified_idxs.push_back(idx);
+        }
+      }
+      auto partial_schema = Schema(modified_columns);
+
+      /** Apply undo */
+      for (uint32_t i = 0; i < modified_idxs.size(); i++) {
+        values[modified_idxs[i]] = undo_log.tuple_.GetValue(&partial_schema, i);
+      }
+
+      Tuple temp_tuple(values, &table_info->schema_);
+      fmt::print(stderr, "  txn{}@{} {} ts={}\n", txn_id, undo_link->prev_log_idx_,
+                 undo_log.is_deleted_ ? "<del>" : temp_tuple.ToString(&table_info->schema_), undo_log.ts_);
+      undo_link = undo_log.prev_version_;
+    }
+    ++itr;
+  }
 
   // We recommend implementing this function as traversing the table heap and print the version chain. An example output
   // of our reference solution:
--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,6 +11,9 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include "concurrency/transaction.h"
+#include "concurrency/transaction_manager.h"
+#include "execution/execution_common.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
@@ -25,17 +28,60 @@ void SeqScanExecutor::Init() {
 
 auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
   auto predicate = plan_->filter_predicate_;
+  auto txn = exec_ctx_->GetTransaction();
+  auto txn_manager = exec_ctx_->GetTransactionManager();
+  auto read_ts = txn->GetReadTs();
 
   while (!table_iter_ptr_->IsEnd()) {
-    auto next_tuple = table_iter_ptr_->GetTuple();
+    auto [next_meta, next_tuple] = table_iter_ptr_->GetTuple();
     auto next_rid = table_iter_ptr_->GetRID();
     ++(*table_iter_ptr_);
 
-    if (!next_tuple.first.is_deleted_) {
-      *tuple = next_tuple.second;
-      *rid = next_rid;
-      if (predicate == nullptr || predicate->Evaluate(tuple, GetOutputSchema()).GetAs<bool>()) {
-        return true;
+    if ((next_meta.ts_ <= read_ts) || (next_meta.ts_ == txn->GetTransactionTempTs())) {
+      // can be seen by current transaction
+      if (!next_meta.is_deleted_) {
+        *tuple = next_tuple;
+        *rid = next_rid;
+        if (predicate == nullptr || predicate->Evaluate(tuple, GetOutputSchema()).GetAs<bool>()) {
+          return true;
+        }
+      }
+    } else {
+      // need to check undo logs
+      auto undo_link = txn_manager->GetUndoLink(next_rid);  // get the first undo link
+      if (undo_link.has_value() && undo_link->IsValid()) {
+        // only continue if the first undo link is valid, otherwise there's no previous version
+        std::vector<UndoLog> undo_logs;
+        bool has_visible_version = true;  // whether there's a visible version
+        while (true) {
+          if (undo_link.has_value() && undo_link->IsValid()) {
+            // current undo link is valid
+            auto undo_log = txn_manager->GetUndoLog(undo_link.value());
+            if (undo_log.ts_ <= read_ts) {
+              // this version can be seen by current transaction
+              undo_logs.push_back(std::move(undo_log));
+              break;
+            }
+            undo_link = undo_log.prev_version_;
+            undo_logs.push_back(std::move(undo_log));
+          } else {
+            // current undo link is invalid and we still not found a visible version
+            has_visible_version = false;
+            break;
+          }
+        }
+        if (has_visible_version) {
+          // found a visible version, reconstruct the tuple
+          auto reconstruct_result = ReconstructTuple(&plan_->OutputSchema(), next_tuple, next_meta, undo_logs);
+          if (reconstruct_result.has_value()) {
+            auto reconstructed_tuple = reconstruct_result.value();
+            if (predicate == nullptr || predicate->Evaluate(&reconstructed_tuple, GetOutputSchema()).GetAs<bool>()) {
+              *tuple = reconstructed_tuple;
+              *rid = next_rid;
+              return true;
+            }
+          }
+        }
       }
     }
   }
